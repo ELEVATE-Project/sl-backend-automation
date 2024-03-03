174e17cf112364f9d0fc3643d9347e507e05d9ca
rvarahag <60617337+infa-rvarahag@users.noreply.github.com>
2024-03-03 22:47:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MentorED
+ 
---
+++ src/test/java/org/shikshalokam/backend/Test1.java
@@ -0,0 +1,14 @@
+package org.shikshalokam.backend;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+
+
+public class Test1 {
+
+    @Test
+    public void test1()
+    {
+        assertTrue(true,"assertionfailed");
+    }
+}

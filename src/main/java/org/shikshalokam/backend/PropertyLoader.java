b0ffc005a32daf0a3324a882c2f8b08583d18c8e
rvarahag <60617337+infa-rvarahag@users.noreply.github.com>
2024-03-11 20:00:36 +0530

    added logic for -D Params as well

--- src/main/java/org/shikshalokam/backend/PropertyLoader.java
+++ src/main/java/org/shikshalokam/backend/PropertyLoader.java
@@ -1,5 +1,8 @@
 package org.shikshalokam.backend;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,13 +11,22 @@ import java.util.Properties;
 public class PropertyLoader {
 
     public static Properties PROP_LIST = new Properties();
-    
+    private static final Logger logger = LogManager.getLogger(PropertyLoader.class);
 
     public static Properties loadProperties() {
         String currentDirectory = System.getProperty("user.dir");
         System.out.println("Current directory: " + currentDirectory);
-        String filePath = currentDirectory+"\\src\\main\\resources\\config\\automation.properties";
-        System.out.println(filePath);
+        String filePath=null;
+        if (System.getProperty("automationPropertiesFile")==null) {
+            filePath = currentDirectory + "\\src\\main\\resources\\config\\automation.properties";
+            System.out.println("The file path from if flow :" + filePath);
+        }else
+        {
+
+            filePath=System.getProperty("automationPropertiesFile");
+            logger.info("The -D Parameter file location:"+ filePath);
+            System.out.println("The file path from else flow :" + filePath);
+        }
         try {
             FileInputStream propFile = new FileInputStream(filePath);
             PROP_LIST.load(propFile);

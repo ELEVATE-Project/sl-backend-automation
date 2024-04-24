93a8df0644a9bd202e89f0d23727eddb4361f497
rvarahag <60617337+infa-rvarahag@users.noreply.github.com>
2024-04-24 15:29:22 +0530

    Update MentorBase.java
    Updating the file path

--- src/main/java/org/shikshalokam/backend/MentorBase.java
+++ src/main/java/org/shikshalokam/backend/MentorBase.java
@@ -4,7 +4,7 @@ import org.apache.logging.log4j.core.config.Configurator;
 public class MentorBase {
     static{
         PropertyLoader.loadProperties();
-        Configurator.initialize(null, System.getProperty("user.dir")+"\\src\\main\\resources\\config\\log4j2.properties");
+        Configurator.initialize(null, System.getProperty("user.dir")+File.separator+ Paths.get("src","main","resources","config").toString()+File.separator+"log4j2.properties");
     }
 
 

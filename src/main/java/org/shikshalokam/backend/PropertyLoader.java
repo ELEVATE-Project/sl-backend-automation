463790250a593674b8e5671dd72a6efa97182af6
rvarahag <60617337+infa-rvarahag@users.noreply.github.com>
2024-04-24 16:06:01 +0530

    Update PropertyLoader.java

--- src/main/java/org/shikshalokam/backend/PropertyLoader.java
+++ src/main/java/org/shikshalokam/backend/PropertyLoader.java
@@ -7,6 +7,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.io.File;
+import java.nio.file.Paths;
 
 public class PropertyLoader {
 

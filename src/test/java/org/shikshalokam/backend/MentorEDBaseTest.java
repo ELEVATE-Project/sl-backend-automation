b0aed8e88ee09a1c39ec5f8286bd5d5db6cda774
rvarahag <varahagiri@gmail.com>
2024-05-26 16:20:22 +0530

    DeleteMenteeAPI adding for END to END flow
    Delete Mentee API adding for END to END flow

--- src/test/java/org/shikshalokam/backend/MentorEDBaseTest.java
+++ src/test/java/org/shikshalokam/backend/MentorEDBaseTest.java
@@ -7,6 +7,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Base64;
 
 import static io.restassured.RestAssured.given;
 
@@ -15,6 +17,10 @@ public class MentorEDBaseTest extends MentorBase {
     private static final Logger logger = LogManager.getLogger(MentorEDBaseTest.class);
     public static String X_AUTH_TOKEN = null;
 
+    private String searchMenteeEndPoint = "mentoring/v1/users/list?type=mentee&page=1&limit=2&search=";
+    private String deleteUseruserEndPoint="user/v1/admin/deleteUser/";
+
+    private String deleteUserMentorEndPoint ="/mentoring/v1/admin/userDelete?userId=";
     public void loginToMentorED(String loginId, String password) {
 
         try {
@@ -36,6 +42,33 @@ public class MentorEDBaseTest extends MentorBase {
 
             logger.info(e.getMessage());
             e.printStackTrace();
+        }
+
+    }
+
+    public void deleteMenteeByGivenName( String name)
+    {
+        String menteeUserId=null;
+        String encodedString = Base64.getEncoder().encodeToString(name.getBytes());
+        searchMenteeEndPoint =  new StringBuilder(searchMenteeEndPoint).append(encodedString).toString();
+        Response responce = given().header("X-auth-token", "bearer " + X_AUTH_TOKEN).when().get(searchMenteeEndPoint);
+        if (responce.getStatusCode()==200)
+        {
+
+            menteeUserId=responce.getBody().jsonPath().get("result.data.values.id" ).toString().split("\\[\\[|\\]\\]")[1];
+            loginToMentorED("adminmaster@admin.com","password");
+            try {
+                responce = given().header("X-auth-token", "bearer " + X_AUTH_TOKEN).when().delete(new URI(deleteUseruserEndPoint + menteeUserId));
+                logger.info("Mentee User  deleted from the User service status :" + responce.getStatusCode());
+                responce = given().header("X-auth-token ", "bearer " + X_AUTH_TOKEN).when().delete(new URI(deleteUserMentorEndPoint + menteeUserId));
+                logger.info("Mentee User  deleted from the Mentor service status :" + responce.getStatusCode());
+            }
+            catch (URISyntaxException e)
+            {
+                logger.info ("User deleteion flow URI is not defined properly");
+            }
+        }else {
+            logger.info("User not found for the given username :" +name);
         }
 
     }

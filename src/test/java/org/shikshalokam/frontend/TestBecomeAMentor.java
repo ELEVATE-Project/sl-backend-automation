d03b6198a7f39508d3ce4e0c06a9c16cccc554e2
Prashantkumar Khatri <khatri2105104@st.jmi.ac.in>
2024-07-30 13:08:47 +0530

    Added flow for administrator while executing become mentor

--- src/test/java/org/shikshalokam/frontend/TestBecomeAMentor.java
+++ src/test/java/org/shikshalokam/frontend/TestBecomeAMentor.java
@@ -1,7 +1,8 @@
 package org.shikshalokam.frontend;
 
-import org.shikshalokam.backend.PropertyLoader;
 import org.shikshalokam.uiPageObjects.AppAllPages;
+import org.shikshalokam.uiPageObjects.PWBasePage;
+import org.shikshalokam.uiPageObjects.PWBrowser;
 import org.shikshalokam.uiPageObjects.Robot;
 import org.testng.annotations.Test;
 
@@ -18,10 +19,28 @@ public class TestBecomeAMentor {
         robot.sees(AppAllPages.welcomePage).becomeMentor();
         robot.sees(AppAllPages.mentorPage).requestToBeAMentor();
         robot.sees(AppAllPages.welcomePage).logOutFromApp();
-        robot.sees(AppAllPages.loginPage).loginToApp(fetchProperty("defaultorgadmin.user"), fetchProperty("defaultorgadmin.password"));
-        robot.sees(AppAllPages.welcomePage).workspace();
-        robot.sees(AppAllPages.workspacePage).acceptAsMentor();
-        robot.sees(AppAllPages.welcomePage).logOutFromApp();
+        if (PWBasePage.PWBrowser == PWBrowser.chromePixel4a || PWBasePage.PWBrowser == PWBrowser.msedgePixel4a) {
+                // Reinitialize the browser for the admin tasks
+                PWBasePage.setBrowserType(PWBrowser.chromium.toString());
+                PWBasePage.reInitializePage();
+
+                Robot adminRobot = new Robot();
+                adminRobot.openApp();
+                adminRobot.sees(AppAllPages.loginPage).loginToApp(fetchProperty("defaultorgadmin.user"),
+                        fetchProperty("defaultorgadmin.password"));
+                adminRobot.sees(AppAllPages.welcomePage).workspace();
+                adminRobot.sees(AppAllPages.workspacePage).acceptAsMentor();
+                adminRobot.sees(AppAllPages.welcomePage).logOutFromApp();
+                adminRobot.quitAppBrowser();
+                PWBasePage.setDefaultBrowserType();
+                robot.openApp();
+        }
+        else {
+            robot.sees(AppAllPages.loginPage).loginToApp(fetchProperty("defaultorgadmin.user"), fetchProperty("defaultorgadmin.password"));
+            robot.sees(AppAllPages.welcomePage).workspace();
+            robot.sees(AppAllPages.workspacePage).acceptAsMentor();
+            robot.sees(AppAllPages.welcomePage).logOutFromApp();
+        }
         robot.sees(AppAllPages.loginPage).loginToApp(fetchProperty("gmailsignup.userEmail"),
                 fetchProperty("gmailsignup.userPassword"));
         robot.sees(AppAllPages.welcomePage).verifiedUserAsaMentor();

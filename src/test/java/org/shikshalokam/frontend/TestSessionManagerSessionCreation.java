fb8446510079b4b6bca665ced2a3156f95a4aaad
Prashantkumar Khatri <khatri2105104@st.jmi.ac.in>
2024-08-14 15:06:58 +0530

    Reverted the changes of TestSessionManagerSessionCreation

--- src/test/java/org/shikshalokam/frontend/TestSessionManagerSessionCreation.java
+++ src/test/java/org/shikshalokam/frontend/TestSessionManagerSessionCreation.java
@@ -1,8 +1,6 @@
 package org.shikshalokam.frontend;
 
 import org.shikshalokam.uiPageObjects.AppAllPages;
-import org.shikshalokam.uiPageObjects.PWBasePage;
-import org.shikshalokam.uiPageObjects.PWBrowser;
 import org.shikshalokam.uiPageObjects.Robot;
 import org.testng.annotations.Test;
 
@@ -14,40 +12,16 @@ public class TestSessionManagerSessionCreation {
     public void testPrivateSessionCreation() {
         Robot robot = new Robot();
         robot.openApp();
-        if (PWBasePage.PWBrowser == PWBrowser.chromePixel4a || PWBasePage.PWBrowser == PWBrowser.msedgePixel4a) {
-            // Reinitialize the browser for the admin tasks
-            PWBasePage.setBrowserType(PWBrowser.chromium.toString());
-            PWBasePage.reInitializePage();
-
-            Robot adminRobot = new Robot();
-            adminRobot.openApp();
-            adminRobot.sees(AppAllPages.loginPage).loginToApp(fetchProperty("defaultorgadmin.user"),
-                    fetchProperty("defaultorgadmin.password"));
-            adminRobot.sees(AppAllPages.welcomePage).workspace();
-            adminRobot.sees(AppAllPages.workspacePage).manageSession();
-            adminRobot.sees(AppAllPages.welcomePage).createSession();
-            adminRobot.sees(AppAllPages.createSessionPage).creatingPrivateSession();
-            adminRobot.sees(AppAllPages.sessionDeatilsPage).verifyAddedMentorName(createSessionPage.addMentorName);
-            adminRobot.sees(AppAllPages.sessionDeatilsPage).verifyMenteeCount("1");
-            adminRobot.sees(AppAllPages.sessionDeatilsPage).verifyMenteeListName(createSessionPage.addMenteeName);
-            adminRobot.sees(AppAllPages.welcomePage).backToWorkSpace();
-            adminRobot.sees(AppAllPages.welcomePage).logOutFromApp();
-            adminRobot.quitAppBrowser();
-            PWBasePage.setDefaultBrowserType();
-            robot.openApp();
-        }
-        else {
-            robot.sees(AppAllPages.loginPage).loginToApp(fetchProperty("defaultorgadmin.user"),
-                    fetchProperty("defaultorgadmin.password"));
-            robot.sees(AppAllPages.welcomePage).workspace();
-            robot.sees(AppAllPages.workspacePage).manageSession();
-            robot.sees(AppAllPages.welcomePage).createSession();
-            robot.sees(AppAllPages.createSessionPage).creatingPrivateSession();
-            robot.sees(AppAllPages.sessionDeatilsPage).verifyAddedMentorName(createSessionPage.addMentorName);
-            robot.sees(AppAllPages.sessionDeatilsPage).verifyMenteeCount("1");
-            robot.sees(AppAllPages.sessionDeatilsPage).verifyMenteeListName(createSessionPage.addMenteeName);
-            robot.sees(AppAllPages.welcomePage).logOutFromApp();
-        }
+        robot.sees(AppAllPages.loginPage).loginToApp(fetchProperty("defaultorgadmin.user"),
+                fetchProperty("defaultorgadmin.password"));
+        robot.sees(AppAllPages.welcomePage).workspace();
+        robot.sees(AppAllPages.workspacePage).manageSession();
+        robot.sees(AppAllPages.welcomePage).createSession();
+        robot.sees(AppAllPages.createSessionPage).creatingPrivateSession();
+        robot.sees(AppAllPages.sessionDeatilsPage).verifyAddedMentorName(createSessionPage.addMentorName);
+        robot.sees(AppAllPages.sessionDeatilsPage).verifyMenteeCount("1");
+        robot.sees(AppAllPages.sessionDeatilsPage).verifyMenteeListName(createSessionPage.addMenteeName);
+        robot.sees(AppAllPages.welcomePage).logOutFromApp();
 
         robot.sees(AppAllPages.loginPage).loginToApp(fetchProperty("default.mentor.user"),
                 fetchProperty("default.mentor.password"));
@@ -61,34 +35,14 @@ public class TestSessionManagerSessionCreation {
         robot.sees(AppAllPages.welcomePage).verifyTags(createSessionPage.privateSessionTitle, "Invited");
         robot.sees(AppAllPages.welcomePage).logOutFromApp();
 
-        if (PWBasePage.PWBrowser == PWBrowser.chromePixel4a || PWBasePage.PWBrowser == PWBrowser.msedgePixel4a) {
-            // Reinitialize the browser for the admin tasks
-            PWBasePage.setBrowserType(PWBrowser.chromium.toString());
-            PWBasePage.reInitializePage();
-
-            Robot adminRobot = new Robot();
-            adminRobot.openApp();
-            adminRobot.sees(AppAllPages.loginPage).loginToApp(fetchProperty("defaultorgadmin.user"),
-                    fetchProperty("defaultorgadmin.password"));
-            adminRobot.sees(AppAllPages.welcomePage).workspace();
-            adminRobot.sees(AppAllPages.workspacePage).manageSession();
-            adminRobot.sees(AppAllPages.welcomePage).selectSessionFromList(createSessionPage.privateSessionTitle);
-            adminRobot.sees(AppAllPages.sessionDeatilsPage).deleteSession();
-            adminRobot.sees(AppAllPages.welcomePage).logOutFromApp();
-            adminRobot.quitAppBrowser();
-            PWBasePage.setDefaultBrowserType();
-        }
-        else {
-            robot.sees(AppAllPages.loginPage).loginToApp(fetchProperty("defaultorgadmin.user"),
-                    fetchProperty("defaultorgadmin.password"));
-            robot.sees(AppAllPages.welcomePage).workspace();
-            robot.sees(AppAllPages.workspacePage).manageSession();
-            robot.sees(AppAllPages.welcomePage).selectSessionFromList(createSessionPage.privateSessionTitle);
-            robot.sees(AppAllPages.sessionDeatilsPage).deleteSession();
-            robot.sees(AppAllPages.welcomePage).logOutFromApp();
-            robot.quitAppBrowser();
-        }
-
+        robot.sees(AppAllPages.loginPage).loginToApp(fetchProperty("defaultorgadmin.user"),
+                fetchProperty("defaultorgadmin.password"));
+        robot.sees(AppAllPages.welcomePage).workspace();
+        robot.sees(AppAllPages.workspacePage).manageSession();
+        robot.sees(AppAllPages.welcomePage).selectSessionFromList(createSessionPage.privateSessionTitle);
+        robot.sees(AppAllPages.sessionDeatilsPage).deleteSession();
+        robot.sees(AppAllPages.welcomePage).logOutFromApp();
+        robot.quitAppBrowser();
 
     }
 
@@ -96,37 +50,15 @@ public class TestSessionManagerSessionCreation {
     public void testPublicSessionCreation() {
         Robot robot = new Robot();
         robot.openApp();
-        if (PWBasePage.PWBrowser == PWBrowser.chromePixel4a || PWBasePage.PWBrowser == PWBrowser.msedgePixel4a) {
-            // Reinitialize the browser for the admin tasks
-            PWBasePage.setBrowserType(PWBrowser.chromium.toString());
-            PWBasePage.reInitializePage();
-
-            Robot adminRobot = new Robot();
-            adminRobot.openApp();
-            adminRobot.sees(AppAllPages.loginPage).loginToApp(fetchProperty("defaultorgadmin.user"),
-                    fetchProperty("defaultorgadmin.password"));
-            adminRobot.sees(AppAllPages.welcomePage).workspace();
-            adminRobot.sees(AppAllPages.workspacePage).manageSession();
-            adminRobot.sees(AppAllPages.welcomePage).createSession();
-            adminRobot.sees(AppAllPages.createSessionPage).creatingPublicSession();
-            adminRobot.sees(AppAllPages.sessionDeatilsPage).verifyAddedMentorName(createSessionPage.addMentorName);
-            adminRobot.sees(AppAllPages.sessionDeatilsPage).verifyMenteeCount("0");
-            adminRobot.sees(AppAllPages.welcomePage).logOutFromApp();
-            adminRobot.quitAppBrowser();
-            PWBasePage.setDefaultBrowserType();
-            robot.openApp();
-        }
-        else {
-            robot.sees(AppAllPages.loginPage).loginToApp(fetchProperty("defaultorgadmin.user"),
-                    fetchProperty("defaultorgadmin.password"));
-            robot.sees(AppAllPages.welcomePage).workspace();
-            robot.sees(AppAllPages.workspacePage).manageSession();
-            robot.sees(AppAllPages.welcomePage).createSession();
-            robot.sees(AppAllPages.createSessionPage).creatingPublicSession();
-            robot.sees(AppAllPages.sessionDeatilsPage).verifyAddedMentorName(createSessionPage.addMentorName);
-            robot.sees(AppAllPages.sessionDeatilsPage).verifyMenteeCount("0");
-            robot.sees(AppAllPages.welcomePage).logOutFromApp();
-        }
+        robot.sees(AppAllPages.loginPage).loginToApp(fetchProperty("defaultorgadmin.user"),
+                fetchProperty("defaultorgadmin.password"));
+        robot.sees(AppAllPages.welcomePage).workspace();
+        robot.sees(AppAllPages.workspacePage).manageSession();
+        robot.sees(AppAllPages.welcomePage).createSession();
+        robot.sees(AppAllPages.createSessionPage).creatingPublicSession();
+        robot.sees(AppAllPages.sessionDeatilsPage).verifyAddedMentorName(createSessionPage.addMentorName);
+        robot.sees(AppAllPages.sessionDeatilsPage).verifyMenteeCount("0");
+        robot.sees(AppAllPages.welcomePage).logOutFromApp();
 
         robot.sees(AppAllPages.loginPage).loginToApp(fetchProperty("default.mentor.user"),
                 fetchProperty("default.mentor.password"));
@@ -134,40 +66,16 @@ public class TestSessionManagerSessionCreation {
         robot.sees(AppAllPages.welcomePage).verifyTags(createSessionPage.publicSessionTitle, "Assigned");
         robot.sees(AppAllPages.welcomePage).logOutFromApp();
 
-        if (PWBasePage.PWBrowser == PWBrowser.chromePixel4a || PWBasePage.PWBrowser == PWBrowser.msedgePixel4a) {
-            // Reinitialize the browser for the admin tasks
-            PWBasePage.setBrowserType(PWBrowser.chromium.toString());
-            PWBasePage.reInitializePage();
-
-            Robot adminRobot = new Robot();
-            adminRobot.openApp();
-            adminRobot.sees(AppAllPages.loginPage).loginToApp(fetchProperty("defaultorgadmin.user"),
-                    fetchProperty("defaultorgadmin.password"));
-            adminRobot.sees(AppAllPages.welcomePage).workspace();
-            adminRobot.sees(AppAllPages.workspacePage).manageSession();
-            adminRobot.sees(AppAllPages.welcomePage).selectSessionFromList(createSessionPage.publicSessionTitle);
-            adminRobot.sees(AppAllPages.sessionDeatilsPage).editSession();
-            adminRobot.sees(AppAllPages.createSessionPage).addMentee();
-            adminRobot.sees(AppAllPages.sessionDeatilsPage).verifyMenteeCount("1");
-            adminRobot.sees(AppAllPages.sessionDeatilsPage).verifyMenteeListName(createSessionPage.addMenteeName);
-            adminRobot.sees(AppAllPages.welcomePage).backToWorkSpace();
-            adminRobot.sees(AppAllPages.welcomePage).logOutFromApp();
-            adminRobot.quitAppBrowser();
-            PWBasePage.setDefaultBrowserType();
-            robot.openApp();
-        }
-        else {
-            robot.sees(AppAllPages.loginPage).loginToApp(fetchProperty("defaultorgadmin.user"),
-                    fetchProperty("defaultorgadmin.password"));
-            robot.sees(AppAllPages.welcomePage).workspace();
-            robot.sees(AppAllPages.workspacePage).manageSession();
-            robot.sees(AppAllPages.welcomePage).selectSessionFromList(createSessionPage.publicSessionTitle);
-            robot.sees(AppAllPages.sessionDeatilsPage).editSession();
-            robot.sees(AppAllPages.createSessionPage).addMentee();
-            robot.sees(AppAllPages.sessionDeatilsPage).verifyMenteeCount("1");
-            robot.sees(AppAllPages.sessionDeatilsPage).verifyMenteeListName(createSessionPage.addMenteeName);
-            robot.sees(AppAllPages.welcomePage).logOutFromApp();
-        }
+        robot.sees(AppAllPages.loginPage).loginToApp(fetchProperty("defaultorgadmin.user"),
+                fetchProperty("defaultorgadmin.password"));
+        robot.sees(AppAllPages.welcomePage).workspace();
+        robot.sees(AppAllPages.workspacePage).manageSession();
+        robot.sees(AppAllPages.welcomePage).selectSessionFromList(createSessionPage.publicSessionTitle);
+        robot.sees(AppAllPages.sessionDeatilsPage).editSession();
+        robot.sees(AppAllPages.createSessionPage).addMentee();
+        robot.sees(AppAllPages.sessionDeatilsPage).verifyMenteeCount("1");
+        robot.sees(AppAllPages.sessionDeatilsPage).verifyMenteeListName(createSessionPage.addMenteeName);
+        robot.sees(AppAllPages.welcomePage).logOutFromApp();
 
         robot.sees(AppAllPages.loginPage).loginToApp(fetchProperty("default.mentee.user"),
                 fetchProperty("default.mentee.password"));
@@ -175,33 +83,13 @@ public class TestSessionManagerSessionCreation {
         robot.sees(AppAllPages.welcomePage).verifyTags(createSessionPage.publicSessionTitle, "Invited");
         robot.sees(AppAllPages.welcomePage).logOutFromApp();
 
-
-        if (PWBasePage.PWBrowser == PWBrowser.chromePixel4a || PWBasePage.PWBrowser == PWBrowser.msedgePixel4a) {
-            // Reinitialize the browser for the admin tasks
-            PWBasePage.setBrowserType(PWBrowser.chromium.toString());
-            PWBasePage.reInitializePage();
-
-            Robot adminRobot = new Robot();
-            adminRobot.openApp();
-            adminRobot.sees(AppAllPages.loginPage).loginToApp(fetchProperty("defaultorgadmin.user"),
-                    fetchProperty("defaultorgadmin.password"));
-            adminRobot.sees(AppAllPages.welcomePage).workspace();
-            adminRobot.sees(AppAllPages.workspacePage).manageSession();
-            adminRobot.sees(AppAllPages.welcomePage).selectSessionFromList(createSessionPage.publicSessionTitle);
-            adminRobot.sees(AppAllPages.sessionDeatilsPage).deleteSession();
-            adminRobot.sees(AppAllPages.welcomePage).logOutFromApp();
-            adminRobot.quitAppBrowser();
-            PWBasePage.setDefaultBrowserType();
-        }
-        else {
-            robot.sees(AppAllPages.loginPage).loginToApp(fetchProperty("defaultorgadmin.user"),
-                    fetchProperty("defaultorgadmin.password"));
-            robot.sees(AppAllPages.welcomePage).workspace();
-            robot.sees(AppAllPages.workspacePage).manageSession();
-            robot.sees(AppAllPages.welcomePage).selectSessionFromList(createSessionPage.publicSessionTitle);
-            robot.sees(AppAllPages.sessionDeatilsPage).deleteSession();
-            robot.sees(AppAllPages.welcomePage).logOutFromApp();
-            robot.quitAppBrowser();
-        }
+        robot.sees(AppAllPages.loginPage).loginToApp(fetchProperty("defaultorgadmin.user"),
+                fetchProperty("defaultorgadmin.password"));
+        robot.sees(AppAllPages.welcomePage).workspace();
+        robot.sees(AppAllPages.workspacePage).manageSession();
+        robot.sees(AppAllPages.welcomePage).selectSessionFromList(createSessionPage.publicSessionTitle);
+        robot.sees(AppAllPages.sessionDeatilsPage).deleteSession();
+        robot.sees(AppAllPages.welcomePage).logOutFromApp();
+        robot.quitAppBrowser();
     }
 }

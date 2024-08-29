e1e6de2b61e286fc6963d2147166072e1c6e8905
Prashantkumar Khatri <khatri2105104@st.jmi.ac.in>
2024-08-29 15:41:32 +0530

    Test Suit fail fixes

--- src/test/java/org/shikshalokam/frontend/TestOtherOptionOnProfile.java
+++ src/test/java/org/shikshalokam/frontend/TestOtherOptionOnProfile.java
@@ -22,11 +22,17 @@ public class TestOtherOptionOnProfile {
         robot.sees(AppAllPages.profileDetailsPage).otherOption("Your expertise");
         robot.sees(AppAllPages.profileDetailsPage).otherOptionValuePopUp("Enter your expertise","Management");
         robot.sees(AppAllPages.profileDetailsPage).submit();
+        if (PWBasePage.PWBrowser == PWBrowser.chromePixel4a || PWBasePage.PWBrowser == PWBrowser.msedgePixel4a) {
+            robot.sees(AppAllPages.welcomePage).backToWorkSpace();
+        }
         robot.sees(AppAllPages.welcomePage).profile();
         robot.sees(AppAllPages.profileDetailsPage).editProfile();
         robot.sees(AppAllPages.profileDetailsPage).verifyAddedOptionIsSelectedAndUnselect("Principle");
         robot.sees(AppAllPages.profileDetailsPage).verifyAddedOptionIsSelectedAndUnselect("Management");
         robot.sees(AppAllPages.profileDetailsPage).submit();
+        if (PWBasePage.PWBrowser == PWBrowser.chromePixel4a || PWBasePage.PWBrowser == PWBrowser.msedgePixel4a) {
+            robot.sees(AppAllPages.welcomePage).backToWorkSpace();
+        }
         robot.sees(AppAllPages.welcomePage).profile();
         robot.sees(AppAllPages.profileDetailsPage).editProfile();
         robot.sees(AppAllPages.profileDetailsPage).verifyAddedOptionIsRemoved("Principle");

be241b96317703aa84dc4778e1dd5caf0b43a5b4
SuhasVMM <167859239+SuhasVMM@users.noreply.github.com>
2024-10-17 00:48:53 +0530

    Added relevant properties in Automation_ElevateAPI_QA.properties
    Added required CSV files for validation of bulk upload entity types bulk_upload_enitytype_update_invalid_ElevateProject.csv, bulk_upload_enitytype_update_valid_ElevateProject.csv, bulk_upload_entity_type_create_ElevateProject.csv
    
    added internal access token inside the class in ElevateProjectBaseTest.java for generic use
    
    added method for uploading a csv file in TestElevateBulkUploadEntityTypeCreate.java and added positive and negative TC's
    
    added positive and negative TC's in TestElevateBulkUploadEntityTypeUpdate.java

--- src/test/java/org/shikshalokam/backend/ep/ElevateProjectBaseTest.java
+++ src/test/java/org/shikshalokam/backend/ep/ElevateProjectBaseTest.java
@@ -16,9 +16,8 @@ public class ElevateProjectBaseTest extends MentorBase {
     static final Logger logger = LogManager.getLogger(ElevateProjectBaseTest.class);
     public static String X_AUTH_TOKEN = null;
     public static Response response = null;
-    public static String User_ID = null;
     public static String internalAccessToken = "Fqn0m0HQ0gXydRtBCg5l";
-    
+
     // method to login with required parameters
     public static Response loginToElevate(String email, String Password) {
         RestAssured.baseURI = PropertyLoader.PROP_LIST.getProperty("elevate.qa.api.base.url");

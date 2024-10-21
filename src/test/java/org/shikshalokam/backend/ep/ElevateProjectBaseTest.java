ab5dd0ba4e799ad9e2ce8eb8e426078737595987
GaganKG <gaganviratgagan88@gmail.com>
2024-10-21 16:25:33 +0530

    added EP base test

--- src/test/java/org/shikshalokam/backend/ep/ElevateProjectBaseTest.java
+++ src/test/java/org/shikshalokam/backend/ep/ElevateProjectBaseTest.java
@@ -17,9 +17,10 @@ public class ElevateProjectBaseTest extends MentorBase {
     public static String X_AUTH_TOKEN = null;
     public static Response response = null;
     public static String User_ID = null;
+    public static String BASE_URL = PropertyLoader.PROP_LIST.getProperty("elevate.qa.api.base.url");
+    public static String INTERNAL_ACCESS_TOKEN = PropertyLoader.PROP_LIST.getProperty("elevate.internalaccesstoken");
 
-
-    // method to login with required parameters
+    // method to Login with required parameters
     public static Response loginToElevate(String email, String Password) {
         RestAssured.baseURI = PropertyLoader.PROP_LIST.getProperty("elevate.qa.api.base.url");
         try {

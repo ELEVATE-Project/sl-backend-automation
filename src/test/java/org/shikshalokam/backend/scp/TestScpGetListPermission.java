b664ea1566b34b9b346426948186ca01c34c5e67
PAP889 <167860418+PAP889@users.noreply.github.com>
2024-10-24 23:59:22 +0530

    fixedtokenissue

--- src/test/java/org/shikshalokam/backend/scp/TestScpGetListPermission.java
+++ src/test/java/org/shikshalokam/backend/scp/TestScpGetListPermission.java
@@ -1,13 +1,9 @@
 package org.shikshalokam.backend.scp;
 
-import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.shikshalokam.backend.PropertyLoader;
-import org.shikshalokam.frontend.scp.TestScpGetPermissionsAPI;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -15,69 +11,58 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import static io.restassured.RestAssured.given;
+import static org.shikshalokam.backend.PropertyLoader.PROP_LIST;
 
-public class TestScpGetListPermission extends SelfCreationPortalBaseTest{
-    static final Logger logger = LogManager.getLogger(TestScpGetPermissionsAPI.class);
-    public static String X_AUTH_TOKEN = null;
-    public static Response response = null;
+public class TestScpGetListPermission extends SelfCreationPortalBaseTest {
+
+    private static final Logger logger = LogManager.getLogger(TestScpGetListPermission.class);
+    private URI permissionListApiEndpoint;
 
     @BeforeTest
     public void setUp() {
-        // Logging in to get the X-AUTH-TOKEN
-        response = SelfCreationPortalBaseTest.loginToScp(PropertyLoader.PROP_LIST.getProperty("scp.qa.admin.login.user"), PropertyLoader.PROP_LIST.getProperty("scp.qa.admin.login.password"));
-        Assert.assertEquals(response.getStatusCode(), 200, "Login failed!");
+        logger.info("Logging into the application:");
 
-       // X_AUTH_TOKEN = response.jsonPath().getString("token");
-        logger.info("X-AUTH-TOKEN retrieved successfully: " + X_AUTH_TOKEN);
+        // Login handled by parent class method
+        loginToScp(PROP_LIST.get("scp.qa.admin.login.user").toString(),
+                PROP_LIST.get("scp.qa.admin.login.password").toString());
+
+        try {
+            // Initialize permissionListApiEndpoint
+            permissionListApiEndpoint = new URI(PROP_LIST.get("scp.qa.permission.list").toString());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException("Invalid URI for permissionListApiEndpoint", e);
+        }
     }
 
-    // Test for getting permissions
     @Test(description = "Verifies the getPermissions API for a valid user.")
-    public void testGetPermissionsApi() throws URISyntaxException {
+    public void testGetPermissionsApi() {
+        logger.info("Started calling the GetPermissions API with valid token.");
 
-        RestAssured.baseURI = PropertyLoader.PROP_LIST.get("scp.qa.api.base.url").toString();
-        // Sending a GET request to the getPermissions API
+        Response response = getPermissions(X_AUTH_TOKEN);
 
-        response = given()
-                .header("Authorization", "Bearer " + X_AUTH_TOKEN)  // Passing the authentication token
-                .when()
-                .get(new URI(PropertyLoader.PROP_LIST.get("scp.qa.permission.list").toString()))
-                .then()
-                .extract().response();
-
-        // Validate the response status code
+        // Validate response code
         Assert.assertEquals(response.getStatusCode(), 200, "Failed to fetch permissions.");
-
-        // Check that the permissions list is not empty
-//        Assert.assertNotNull(response.jsonPath().getList("permissions"), "Permissions list should not be empty.");
-
-        logger.info("Permissions retrieved successfully: " + response.getBody().asString());
+        logger.info("Permissions retrieved successfully.");
     }
 
-    // Test for invalid token case
     @Test(description = "Verifies the getPermissions API with an invalid token.")
-    public void testGetPermissionsApiWithInvalidToken() throws URISyntaxException {
+    public void testGetPermissionsApiWithInvalidToken() {
+        logger.info("Started calling the GetPermissions API with an invalid token.");
 
-        RestAssured.baseURI = PropertyLoader.PROP_LIST.get("scp.qa.api.base.url").toString();
-        // Sending a GET request with an invalid token
-        X_AUTH_TOKEN = null;
-        Response responseInvalidToken = given()
-                .header("X-auth-token", "bearer token")
-                .log().all() // Passing an invalid token
+        Response response = getPermissions("invalid token");
+
+        // Validate response code for invalid token
+        Assert.assertEquals(response.getStatusCode(), 401, "Expected 401 Unauthorized error.");
+        logger.info("Received expected error for invalid token.");
+    }
+
+    // Method to handle the getPermissions API request
+    private Response getPermissions(String token) {
+        return given()
+                .header("X-auth-token", "bearer " + token)
                 .when()
-                .get(new URI(PropertyLoader.PROP_LIST.get("scp.qa.permission.list").toString()))
+                .get(permissionListApiEndpoint)
                 .then()
-
                 .extract().response();
-
-        responseInvalidToken.prettyPrint();
-
-        System.out.println("Response Status Code: " + responseInvalidToken.getStatusCode());
-        System.out.println("Response Body: " + responseInvalidToken.getBody().asString());
-
-        // Validate the response status code
-        Assert.assertEquals(responseInvalidToken.getStatusCode(), 401, "Expected a 401 Unauthorized error for invalid token.");
-
-        logger.info("Received expected error for invalid token: " + responseInvalidToken.getBody().asString());
     }
 }

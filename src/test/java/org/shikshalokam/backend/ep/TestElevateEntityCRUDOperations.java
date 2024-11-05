0c173669b4a773fe5a3f296c341835d7118f7b0a
Shaikjubedha <jubedha@shikshalokam.org>
2024-11-05 21:49:56 +0530

    added code entities

--- src/test/java/org/shikshalokam/backend/ep/TestElevateEntityCRUDOperations.java
+++ src/test/java/org/shikshalokam/backend/ep/TestElevateEntityCRUDOperations.java
@@ -9,38 +9,28 @@ import org.shikshalokam.backend.PropertyLoader;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
 public class TestElevateEntityCRUDOperations extends ElevateProjectBaseTest {
-    private String randomName = RandomStringUtils.randomAlphabetic(10);
-    private String randomExternalId = RandomStringUtils.randomAlphabetic(10);
     private static final Logger logger = LogManager.getLogger(TestElevateEntityCRUDOperations.class);
-
+  private   String randomName = RandomStringUtils.randomAlphabetic(10);
+     private String randomExternalId = RandomStringUtils.randomAlphabetic(10);
     @BeforeMethod
     public void userLogin() {
         loginToElevate(PropertyLoader.PROP_LIST.getProperty("elevate.login.contentcreator"),
                 PropertyLoader.PROP_LIST.getProperty("elevate.login.contentcreator.password"));
     }
 
-    // Method to create an entity with random 'name' and 'externalId'
-    private Response addEntities(String entityTypeId, String name, String externalId) {
-        Response response = given()
-                .header("internal-access-token", INTERNAL_ACCESS_TOKEN)
-                .header("x-auth-token", X_AUTH_TOKEN)
-                .header("Content-Type", "application/json")
-                .body("{ \"entityTypeId\": \"" + entityTypeId + "\", " +
-                        "\"name\": \"" + name + "\", " +
-                        "\"externalId\": \"" + externalId + "\" }")
-                .post(PropertyLoader.PROP_LIST.getProperty("elevate.qa.entityadd.endpoint"));
-
-        response.prettyPrint();
-        return response;
-    }
-
     @Test(description = "adding valid entity with random name and externalId")
     public void testAddingSingleEntity() {
+
         Response response = addEntities("someEntityTypeId", randomName, randomExternalId);
+
         Assert.assertEquals(response.getStatusCode(), 200);
         Assert.assertEquals(response.jsonPath().getString("message"), "ENTITY_ADDED");
         logger.info("New entity created with name: " + randomName + " and externalId: " + randomExternalId);
@@ -56,15 +46,29 @@ public class TestElevateEntityCRUDOperations extends ElevateProjectBaseTest {
         String actualMessage = response.jsonPath().getString("message[0].msg");
 
         Assert.assertEquals(actualMessage, expectedMessage);
-        logger.info("Validation testInvalidAddingSingleEntity is verified");
+        logger.info("Validation with empty 'name' field is verified");
+    }
+
+    // Method to create an entity with specified 'name' and 'externalId'
+    private Response addEntities(String entityTypeId, String name, String externalId) {
+        // Create the request body as a Map
+        Map<String, String> requestBody = new HashMap<>();
+        requestBody.put("entityTypeId", entityTypeId);
+        requestBody.put("name", name);
+        requestBody.put("externalId", externalId);
+
+        // Log the request body for debugging
+        logger.info("Request body: " + requestBody);
+
+        // Sending the request
+        Response response = given()
+                .header("internal-access-token", INTERNAL_ACCESS_TOKEN)
+                .header("x-auth-token", X_AUTH_TOKEN)
+                .contentType(ContentType.JSON)
+                .body(requestBody)  // Use the Map directly as the body
+                .post(PropertyLoader.PROP_LIST.getProperty("elevate.qa.entityadd.endpoint"));
+
+        response.prettyPrint(); // Log the response for debugging
+        return response;
     }
 }
-
-
-
-
-
-
-
-
-

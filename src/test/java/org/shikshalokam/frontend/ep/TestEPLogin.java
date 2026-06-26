434ba15be3d4f7fa7cdfcf71c15e3812bb65f8ac
hanumanthpujar <hanumanthpujar52@gmail.com>
2026-06-26 07:50:49 +0530

    LED IMP flow

--- src/test/java/org/shikshalokam/frontend/ep/TestEPLogin.java
+++ src/test/java/org/shikshalokam/frontend/ep/TestEPLogin.java
@@ -7,12 +7,131 @@ import org.testng.annotations.Test;
 import static org.shikshalokam.uiPageObjects.PWBasePage.fetchProperty;
 
 public class TestEPLogin {
-    @Test(description = "Verify log in page working properly.")
+    @Test(priority = 1, description = "Verify log in page working properly.")
     public void testLoginActivity() {
         Robot robot = new Robot();
         robot.sees(AppAllPages.eploginpage).openURL();
         robot.sees(AppAllPages.eploginpage).logIntoPortal();
         robot.sees(AppAllPages.homePage).verifyHomePage();
+        robot.quitAppBrowser();
+    }
+
+    @Test(priority = 2, description = "Verify log in page with Username and password working properly.")
+    public void testLoginWithUserName() {
+        Robot robot = new Robot();
+        String userName = fetchProperty("ep.username");
+        String password = fetchProperty("ep.password");
+        robot.sees(AppAllPages.eploginpage).openURL();
+        robot.sees(AppAllPages.eploginpage).testLoginWithMultipleOptions(userName,password,"Username");
+        robot.sees(AppAllPages.homePage).verifyHomePage();
+        robot.quitAppBrowser();
+    }
+    @Test(priority = 3, description = "Verify log in page with Name and password working properly.")
+    public void testLoginWithPhoneNumber() {
+        Robot robot = new Robot();
+        String userName = fetchProperty("ep.phoneNumber");
+        String password = fetchProperty("ep.password");
+        robot.sees(AppAllPages.eploginpage).openURL();
+        robot.sees(AppAllPages.eploginpage).testLoginWithMultipleOptions(userName,password,"Phone Number");
+        robot.sees(AppAllPages.homePage).verifyHomePage();
+        robot.quitAppBrowser();
+    }
+
+//Negative test cases for login with wrong credentials
+    @Test(priority = 4, description = "Verify log in page with Name and password working.")
+    public void testLoginWithName() {
+        Robot robot = new Robot();
+        String userName = fetchProperty("ep.nameoftheuser");
+        String password = fetchProperty("ep.password");
+        robot.sees(AppAllPages.eploginpage).openURL();
+        robot.sees(AppAllPages.eploginpage).testLoginWithMultipleOptions(userName,password,"Negative test");
+        robot.quitAppBrowser();
+    }
+
+    @Test(priority = 5, description = "Verify log in page with Wrong Phone Number and password.")
+    public void testLoginWithWrongPhoneNumber() {
+        Robot robot = new Robot();
+        String password = fetchProperty("ep.password");
+        robot.sees(AppAllPages.eploginpage).openURL();
+        robot.sees(AppAllPages.eploginpage).testLoginWithMultipleOptions("9999999991",password,"Negative test");
+        robot.quitAppBrowser();
+    }
+    @Test(priority = 6, description = "Verify log in page with Empty User Name.")
+    public void testLoginWithEmptyUserName() {
+        Robot robot = new Robot();
+        String password = fetchProperty("ep.password");
+        robot.sees(AppAllPages.eploginpage).openURL();
+        robot.sees(AppAllPages.eploginpage)
+                .testLoginWithMultipleOptions("", password, "Negative test");
+        robot.quitAppBrowser();
+    }
+
+    @Test(priority = 7, description = "Verify log in page with Empty Password.")
+    public void testLoginWithEmptyPassword() {
+        Robot robot = new Robot();
+        String userName = fetchProperty("ep.username");
+        robot.sees(AppAllPages.eploginpage).openURL();
+        robot.sees(AppAllPages.eploginpage)
+                .testLoginWithMultipleOptions(userName, "", "Negative test");
+        robot.quitAppBrowser();
+    }
+
+    @Test(priority = 8, description = "Verify log in page with Empty User Name and Password.")
+    public void testLoginWithEmptyCredentials() {
+        Robot robot = new Robot();
+        robot.sees(AppAllPages.eploginpage).openURL();
+        robot.sees(AppAllPages.eploginpage)
+                .testLoginWithMultipleOptions("", "", "Negative test");
+        robot.quitAppBrowser();
+    }
+
+    @Test(priority = 9, description = "Verify log in page with Wrong User Name and Wrong Password.")
+    public void testLoginWithInvalidCredentials() {
+        Robot robot = new Robot();
+        String userName = fetchProperty("ep.username");
+        String password = fetchProperty("ep.password");
+        robot.sees(AppAllPages.eploginpage).openURL();
+        robot.sees(AppAllPages.eploginpage)
+                .testLoginWithMultipleOptions(userName + "1", password + "1", "Negative test");
+        robot.quitAppBrowser();
+    }
+
+    @Test(priority = 10, description = "Verify log in page with Short Phone Number.")
+    public void testLoginWithShortPhoneNumber() {
+        Robot robot = new Robot();
+        String password = fetchProperty("ep.password");
+        robot.sees(AppAllPages.eploginpage).openURL();
+        robot.sees(AppAllPages.eploginpage)
+                .testLoginWithMultipleOptions("12345", password, "Negative test");
+        robot.quitAppBrowser();
+    }
+
+    @Test(priority = 11, description = "Verify log in page with Long Phone Number.")
+    public void testLoginWithLongPhoneNumber() {
+        Robot robot = new Robot();
+        String password = fetchProperty("ep.password");
+        robot.sees(AppAllPages.eploginpage).openURL();
+        robot.sees(AppAllPages.eploginpage)
+                .testLoginWithMultipleOptions("123456789012345", password, "Negative test");
+        robot.quitAppBrowser();
+    }
+
+    @Test(priority = 12, description = "Verify log in page with User Name and Wrong password.")
+    public void testLoginWithWrongPassword() {
+        Robot robot = new Robot();
+        String userName = fetchProperty("ep.username");
+        String password = fetchProperty("ep.password");
+        robot.sees(AppAllPages.eploginpage).openURL();
+        robot.sees(AppAllPages.eploginpage).testLoginWithMultipleOptions(userName+"1",password,"Negative test");
+        robot.quitAppBrowser();
+    }
+    @Test(priority = 13, description = "Verify log in page with Wrong email and password.")
+    public void testLoginWithWrongUserName() {
+        Robot robot = new Robot();
+        String userName = fetchProperty("ep.mail");
+        String password = fetchProperty("ep.password");
+        robot.sees(AppAllPages.eploginpage).openURL();
+        robot.sees(AppAllPages.eploginpage).testLoginWithMultipleOptions(userName,password+"1","Negative test");
         robot.quitAppBrowser();
     }
 }
--- src/test/java/org/shikshalokam/uiPageObjects/ep/AppEPLoginPage.java
+++ src/test/java/org/shikshalokam/uiPageObjects/ep/AppEPLoginPage.java
@@ -1,5 +1,6 @@
 package org.shikshalokam.uiPageObjects.ep;
 
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
 import org.apache.logging.log4j.LogManager;
@@ -8,6 +9,10 @@ import org.shikshalokam.backend.PropertyLoader;
 import org.shikshalokam.uiPageObjects.AppLoginPage;
 import org.shikshalokam.uiPageObjects.PWBasePage;
 import org.shikshalokam.uiPageObjects.Robot;
+import org.testng.Assert;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static org.shikshalokam.backend.TestCRUDUserPermissions.logger;
 
 public class AppEPLoginPage extends PWBasePage {
     AppEPLoginPage login;
@@ -23,9 +28,8 @@ public class AppEPLoginPage extends PWBasePage {
         PWBasePage.page.navigate(PropertyLoader.PROP_LIST.getProperty("ep.url"));
     }
 
-
     public AppEPLoginPage logIntoPortal() {
-        logger.info("login started");
+        logger.info("login with email started");
         String userName = fetchProperty("ep.mail");
         String password = fetchProperty("ep.password");
         page.locator("//input[@name='login-username']").click();
@@ -33,10 +37,22 @@ public class AppEPLoginPage extends PWBasePage {
         page.locator("//input[@name='login-password']").click();
         page.locator("//input[@name='login-password']").fill(password);
         page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login")).click();
+        logger.info("login with email started ended");
+        return login;
+    }
 
-        logger.info("login ended");
-
-
+    public AppEPLoginPage testLoginWithMultipleOptions(String userName,String password,String loggerMessage) {
+        logger.info("login with "+loggerMessage+" started");
+        page.locator("//input[@name='login-username']").click();
+        page.locator("//input[@name='login-username']").fill(userName);
+        page.locator("//input[@name='login-password']").click();
+        page.locator("//input[@name='login-password']").fill(password);
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login")).click();
+        if(loggerMessage.equals("Negative test"))
+        {
+            assertThat(page.getByText("Welcome")).not().isVisible();
+        }
+        logger.info("login with "+loggerMessage+" ended");
         return login;
     }
 }
